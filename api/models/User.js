efd259a3eb4317ac43bbee5d7ca6e43f22935ba2
essamaboelmgd1 <essamaboelmgd1@gmail.com>
2025-11-09 23:16:46 +0200

    update 1

--- api/models/User.js
+++ api/models/User.js
@@ -20,8 +20,7 @@ userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
     return next();
   }
-  const salt = await bcrypt.genSalt(12);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
